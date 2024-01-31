3918dc83b201af9a494b2c5382f3fbad62398205
Ankit Khadka <khadkaankit85@gmail.com>
2024-01-31 18:53:12 -0500

    made function so that we can pass different strings

--- TheMirrorMirrorTwo.js
+++ TheMirrorMirrorTwo.js
@@ -1,8 +1,13 @@
 // The Mirror Mirror: Imagine you have a string, and you need to create a new string that is a mirror image of the original. Write a function that appends the reversed version of the original string to itself.
 let ourString="Imagine you have a string, and you need to create a new string that is a mirror image of the original"
 let reversedString=''
-for (let i= (ourString.length-1); i >= 0; i--) {
-    reversedString+=ourString[i]
-    
+
+
+function reverser(str){
+    for (let i= (str.length-1); i >= 0; i--) {
+    reversedString+=str[i]
+    }
+    console.log(str+" "+reversedString)
+
 }
-console.log(ourString+" "+reversedString)
+reverser(ourString)
