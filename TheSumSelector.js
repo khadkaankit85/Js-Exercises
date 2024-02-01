7ff9a3d5a381ee343ebc3b0aa38f20176e8aaf01
Ankit Khadka <khadkaankit85@gmail.com>
2024-01-31 19:58:07 -0500

    sum selector completed

--- TheSumSelector.js
+++ TheSumSelector.js
@@ -1,14 +1,21 @@
 // The Sum Selector: You are working on a function that should sum all numbers in an array until it encounters a negative number. Write a function that performs this summation.
-let numList=[1,2,3,4,-5,6,-8,2,-10,2,20,-1,41,8,5]
+let mylist=[1,2,3,4,-5,6,-8,2,-10,2,20,-1,41,8,5]
 let sum=0
+
+function selectedSum(numList){
 for (let index = 0; index < numList.length; index++) {
     let element = numList[index];
     if(element>=0){
+        console.log(element)
         sum+=element
     }
     if(element<0){
+        console.log("Negative number Detected :",element)
         break;
     }
     
 }
-console.log(sum)
+console.log("The sum is :", sum)
+}
+
+selectedSum(mylist)
